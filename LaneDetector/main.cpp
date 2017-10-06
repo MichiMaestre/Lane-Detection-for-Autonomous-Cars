c000560ab57250df29e04e37354b5dd131dfe8e9
michi <a17maestre2009@gmail.com>
2017-10-05 21:26:54 -0400

    Create repository

---
+++ LaneDetector/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <lib.hpp>
+
+int main()
+{
+    dummy();
+    return 0;
+}
---
+++ test/test.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(dummy, should_pass) {
+  EXPECT_EQ(1, 1);
+}
