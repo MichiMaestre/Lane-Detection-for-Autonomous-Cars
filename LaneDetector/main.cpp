529084457e7b62187ebeb3b62fe8482609c18fcc
michi <a17maestre2009@gmail.com>
2017-10-09 02:07:43 -0400

    SIP Task #4: Initialized video reader in main.cpp. Added OpenCV path to CmakeLists

--- LaneDetector/main.cpp
+++ LaneDetector/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
-#include <lib.hpp>
+#include <LaneDetector.hpp>
+#include "opencv2/opencv.hpp"
+#include <opencv2/highgui/highgui.hpp>
 
-int main()
-{
-    dummy();
+
+int main() {
+
+    cv::VideoCapture cap("/home/michi/Desktop/project_video.mp4");
+    if (!cap.isOpened())
+      return -1;
+
+    cv::Mat frame;
+    cv::namedWindow("Output",CV_WINDOW_AUTOSIZE);
+    while(1) {
+
+      if (!cap.read(frame))
+        break;
+
+      cv::imshow("Output", frame);
+      cv::waitKey(20);
+
+      // Start image processing
+    }
     return 0;
 }
